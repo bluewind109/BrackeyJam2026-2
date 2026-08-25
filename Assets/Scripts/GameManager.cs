390ecf0f3ff25c6bfd0b93fa1fae1d7d967e39f9
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-08-25 22:15:23 +0700

    * Update game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,37 +3,38 @@ using GameStates;
 
 public class GameManager : MonoBehaviour
 {
-	[SerializeField] private Player _player;
-	
-    private GameState _currentState;
-    private GameState _introState;
-    private GameState _gameplayState;
-    private GameState _pauseState;
-    private GameState _gameOverState;
+  [SerializeField] private Player _player;
+  [SerializeField] private Timer _focusTimer;
+
+  private GameState _currentState;
+  private GameState _introState;
+  private GameState _gameplayState;
+  private GameState _pauseState;
+  private GameState _gameOverState;
 
 
-    void Start()
-    {
-		_introState = new IntroState(this);
-		_gameplayState = new GameplayState(this, _player);
-		_pauseState = new PauseState(this);
-		_gameOverState = new GameOverState(this);
+  void Start()
+  {
+    _introState = new IntroState(this);
+    _gameplayState = new GameplayState(this, _player, _focusTimer);
+    _pauseState = new PauseState(this);
+    _gameOverState = new GameOverState(this);
 
-		SetState(_gameplayState);
-    }
+    SetState(_gameplayState);
+  }
 
-    void Update()
-    {
-		_currentState?.Update();
-    }
+  void Update()
+  {
+    _currentState?.Update();
+  }
 
-    private void SetState(GameState newState)
-    {
-        if (newState == null) return;
-        if (_currentState == newState) return;
+  private void SetState(GameState newState)
+  {
+    if (newState == null) return;
+    if (_currentState == newState) return;
 
-        _currentState?.Exit();
-        _currentState = newState;
-        _currentState?.Enter();
-    }
+    _currentState?.Exit();
+    _currentState = newState;
+    _currentState?.Enter();
+  }
 }
