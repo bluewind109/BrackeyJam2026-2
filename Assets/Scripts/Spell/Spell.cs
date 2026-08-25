58777505a2b11d92972473c18349889a6d846e21
Khanh Hoang Quoc <khanh.hoangquoc91@gmail.com>
2026-08-25 17:43:53 +0700

    + Add level to spell

--- Assets/Scripts/Spell/Spell.cs
+++ Assets/Scripts/Spell/Spell.cs
@@ -5,12 +5,14 @@ public abstract class Spell : ScriptableObject
 	[Header("Spell Settings")]
     [SerializeField] private string _spellName;
 	[SerializeField] private Sprite _spellIcon;
+	[SerializeField] private int _level;
 	[SerializeField] private int _damage;
 	[SerializeField] private float _cooldown;
 	[SerializeField] private InputSequence _inputSequence;
 
 	public string SpellName => _spellName;
 	public Sprite SpellIcon => _spellIcon;
+	public int Level => _level;
 	public int Damage => _damage;
 	public float Cooldown => _cooldown;
 	public InputSequence InputSequence => _inputSequence;
