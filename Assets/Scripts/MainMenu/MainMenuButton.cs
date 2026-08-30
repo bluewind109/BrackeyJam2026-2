27af7b61089de5e288eecc1b1588dde782bfb8c0
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-08-30 13:59:24 +0700

    * Update main menu

--- Assets/Scripts/MainMenu/MainMenuButton.cs
+++ Assets/Scripts/MainMenu/MainMenuButton.cs
@@ -2,17 +2,31 @@ using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class MainMenuButton : MonoBehaviour
 {
-    public static Action onPressed;
+    public event Action onPressed;
 
     [SerializeField] private MainMenuActionType actionType;
     [SerializeField] private Image activeBg;
     [SerializeField] private TextMeshProUGUI buttonText;
 
+    private CanvasGroup canvasGroup;
+
     private bool isActive = false;
     public MainMenuActionType ActionType => actionType;
+
+    public void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        if (canvasGroup) PlayTweenAlphaLoop(1f, 0f, 1f);
+    }
+
+    public void PlayTweenAlphaLoop(float duration, float minAlpha, float maxAlpha)
+    {
+        canvasGroup?.DOFade(minAlpha, duration).SetLoops(-1, LoopType.Yoyo);
+    }
 
     public void SetActive(bool active)
     {
